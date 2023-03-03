aa63ebad64c3cf109152930a29c37f44ce233a6d
diegogirao <diegoramiro25@gmail.com>
2023-03-03 17:54:46 -0300

    Feat axios to get API data and render data

--- src/App.js
+++ src/App.js
@@ -1,37 +1,75 @@
-import logo from "./logo.svg"
+import React from "react"
+import axios from "axios"
+
 import "./App.css"
 
-let advices = []
+class App extends React.Component {
+	state = {
+		advice: "",
+	}
+	// fetchingQuote
+	componentDidMount() {
+		this.fetchAvice()
+	}
+	//func to fetch quote
+	fetchAvice = () => {
+		axios
+			.get("https://api.adviceslip.com/advice")
+			.then((response) => {
+				const { advice } = response.data.slip
 
-//Teste de consumo da API AdviceSlip
-const urlAPI = "https://api.adviceslip.com/advice"
-getAdvices()
+				this.setState({ advice })
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
 
-async function getAdvices() {
-	const res = await fetch(urlAPI)
-	advices = await res.json()
-	console.log(advices)
-}
-
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	)
+	render() {
+		const { advice } = this.state
+		return (
+			<div className="app">
+				<div className="card">
+					<h1 className="heading">{advice}</h1>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App
+
+// let advices = []
+
+// //Teste de consumo da API AdviceSlip
+// const urlAPI = "https://api.adviceslip.com/advice"
+// getAdvices()
+
+// async function getAdvices() {
+// 	const res = await fetch(urlAPI)
+// 	advices = await res.json()
+// 	console.log(advices)
+// 	console.table(advices.slip.id)
+// 	console.table(advices.slip.advice)
+// }
+
+// function App() {
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 			</header>
+// 		</div>
+// 	)
+// }
